7a2388adcc10bf4301d586bc78cd15f2b3c46c9d
Melvin Rivera <jylrivera96@gmail.com>
2024-04-17 14:23:06 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Xplore Inspecciones',
+  webDir: 'www'
+};
+
+export default config;
