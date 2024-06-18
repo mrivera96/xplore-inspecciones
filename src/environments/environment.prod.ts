baa263a49ac7b1841b80ccce07708544edea7a61
mrivera96 <jylrivera96@gmail.com>
2024-06-17 19:21:29 -0600

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl:'http://190.4.56.14/inspApiDesa/api',
+  apiUrl:'http://190.4.56.14/inspApi/api',
   cryptKey:'0436b7ac-c152-4839-b054-f5852e06bc02',
-  imagesUrl: 'http://190.4.56.14/inspApiDesa',
+  imagesUrl: 'http://190.4.56.14/inspApi',
   CAPACITOR_ANDROID_STUDIO_PATH: "C:\\Program Files\\Android\\Android Studio\\bin",
 };
